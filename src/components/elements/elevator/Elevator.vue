055796cd67295b7ff4ca9575050e4288ae2d60ba
Kim_Timur <kimutir@gmail.com>
2022-12-15 18:52:56 +0300

    animation added

--- src/components/elements/elevator/Elevator.vue
+++ src/components/elements/elevator/Elevator.vue
@@ -4,7 +4,12 @@
     :style="{ height: elevHeight + 'px' }"
     ref="elevator"
   >
-    <elevator-info :currentFloor="currentFloor" />
+    <elevator-info
+      :waiting="waiting"
+      :active="active"
+      :currentFloor="currentFloor"
+      :queue="queue"
+    />
   </div>
 </template>
 
@@ -13,7 +18,10 @@ import config from "@/config";
 import ElevatorInfo from "@/components/elements/elevator/ElevatorInfo.vue";
 
 export default {
-  props: {},
+  props: {
+    queue: Array,
+    number: Number,
+  },
   components: {
     ElevatorInfo,
   },
@@ -21,7 +29,89 @@ export default {
     return {
       currentFloor: 1,
       elevHeight: window.innerHeight / config.floors,
+      active: false,
+      waiting: false,
+      // требуемая скорость
+      speed: window.innerHeight / config.floors / 1000,
     };
+  },
+  methods: {
+    movementAnimation(floor) {
+      return new Promise((res) => {
+        const start = performance.now();
+
+        // с какой позиции начинается анимация
+        const lastPosition =
+          (config.floors - this.currentFloor) * this.elevHeight;
+
+        const animate = (time) => {
+          // длительность анимации
+          const duraction = time - start;
+          const currentPosotion = this.$refs.elevator.offsetTop;
+          // направление движения (реверсивно)
+          const direction = floor - this.currentFloor < 0 ? -1 : 1;
+
+          this.$refs.elevator.style.top = `${
+            lastPosition - direction * duraction * this.speed
+          }px`;
+
+          // вызываем анимацию пока не доедет до нужного этажа
+          if (
+            currentPosotion * direction >
+            (config.floors - floor) * this.elevHeight * direction
+          ) {
+            requestAnimationFrame(animate);
+          } else {
+            this.$refs.elevator.style.top = `${
+              (config.floors - floor) * this.elevHeight
+            }px`;
+            cancelAnimationFrame(animate);
+            res();
+          }
+        };
+
+        requestAnimationFrame(animate);
+      });
+    },
+    move(queue) {
+      // если стек пустой, заканчиваем движение
+      if (!queue.length) {
+        this.active = false;
+        return;
+      }
+
+      this.movementAnimation(queue[0]).then(() => {
+        // лифт на новом этаже
+        this.currentFloor = queue[0];
+        // удаляем его из стека
+        queue.shift();
+        // остановка 3 секунды и продолжение
+        this.waiting = true;
+        setTimeout(() => {
+          this.waiting = false;
+          // вызываем рекурсивно, пока в стеке есть этажи
+          return this.move(queue);
+        }, 3000);
+      });
+    },
+  },
+  watch: {
+    // наблюдаем за изменением стека
+    queue: {
+      handler(value) {
+        console.log(value);
+        if (!this.active && this.queue.length) {
+          this.active = true;
+          this.move(value);
+        }
+      },
+      deep: true,
+    },
+  },
+  mounted() {
+    // опускаем лифт на первый этаж
+    this.$refs.elevator.style.top =
+      (config.floors - 1) * this.elevHeight + "px";
   },
 };
 </script>
--- src/components/elements/elevator/ElevatorInfo.vue
+++ src/components/elements/elevator/ElevatorInfo.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="elevator__info info">
-    <div class="info__direction"></div>
+    <div class="info__direction" :class="findDirection" />
     <div class="info__floor">
-      {{ currentFloor }}
+      {{ waiting || !queue[0] ? currentFloor : queue[0] }}
     </div>
   </div>
 </template>
@@ -11,6 +11,20 @@
 export default {
   props: {
     currentFloor: Number,
+    queue: Array,
+    active: Boolean,
+    waiting: Boolean,
+  },
+  computed: {
+    // определения класса стрелки
+    findDirection() {
+      if (this.waiting) return "direction__waiting";
+      if (!this.active) return "direction__none";
+      if (this.active && this.queue[0] > this.currentFloor)
+        return "direction__up";
+      if (this.active && this.queue[0] < this.currentFloor)
+        return "direction__down";
+    },
   },
 };
 </script>
@@ -31,5 +45,41 @@ export default {
 
 .info__floor {
   font-size: 23px;
+}
+
+.direction__up {
+  border-top: 3px solid #000;
+  border-right: 3px solid #000;
+  transform: rotate(-45deg);
+}
+.direction__down {
+  border-top: 3px solid #000;
+  border-right: 3px solid #000;
+  transform: rotate(135deg);
+}
+.direction__waiting {
+  background-color: #000;
+  border-radius: 50%;
+  animation-name: blinker;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+  animation-duration: 1.5s;
+}
+
+.direction__none {
+  background-color: #000;
+  border-radius: 50%;
+}
+
+@keyframes blinker {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 </style>
